8710ee7d20f55d589f2a0f914eecb39273391d97
Freet8707 <freet8707@gmail.com>
2020-08-12 23:25:42 -0500

    UpdateMovie form can successfully edit title, director, and metascore values

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -13,6 +13,7 @@ const initialState = {
 const UpdateMovie = (props) => {
     const params = useParams()
     const [movie, setMovie] = useState({})
+    const [stars, setStars] = useState([])
 
     useEffect(() => {
         if(props.movieList.length > 0){
@@ -21,15 +22,75 @@ const UpdateMovie = (props) => {
             })
             console.log(foundMovie)
             setMovie(foundMovie)
+            setStars(foundMovie.stars)
         } else {
             console.log('fetching movie')
         }
     }, [props.movieList])
+
+    const handleMetaChange = e => {
+        if(e.target.name = 'metascore'){
+            setMovie({
+                ...movie,
+                [e.target.name]: parseInt(e.target.value, 10)
+            })
+        } 
+    }
+
+    const handleChange = e => {
+        setMovie({
+            ...movie,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        console.log(movie)
+    }
     
     
     return (
         <>
-            <div>Here is the Movie: {movie.title}</div>
+            <form onSubmit={handleSubmit} className='updateForm'>
+                <div>
+                    <label htmlFor='title' />Movie Title: 
+                    <input
+                        type='text'
+                        id='title'
+                        name='title'
+                        onChange={handleChange}
+                        value={movie.title}
+                    />
+                </div>
+                <div>
+                    <label htmlFor='director' />Movie Director: 
+                    <input
+                        type='text'
+                        id='director'
+                        name='director'
+                        onChange={handleChange}
+                        value={movie.director}
+                    />
+                </div>
+                <div>
+                    <label htmlFor='metascore' />Movie Metascore: 
+                    <input
+                        type='text'
+                        id='metascore'
+                        name='metascore'
+                        onChange={handleMetaChange}
+                        value={movie.metascore}
+                    />
+                </div>
+                <button type='submit'>Update Movie</button>
+                
+            </form>
+            <h3>Stars: </h3>
+            {stars.map(s => {
+                return <p key={s}>{s}</p>
+            })}
         </>
     )
 }
